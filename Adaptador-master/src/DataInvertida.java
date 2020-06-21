986bc32d3015254d1965f15b5514cf36b609a1e0
eloyarnaud <38596357+eloyarnaud@users.noreply.github.com>
2020-06-21 14:08:00 -0300

    Add files via upload

---
+++ Adaptador-master/src/DataInvertida.java
@@ -0,0 +1,15 @@
+
+public class DataInvertida extends DataBrasil {
+
+	public DataInvertida(int ano, int mes, int dia) {
+		this.setDia(dia);
+		this.setMes(mes);
+		this.setAno(ano);
+	}
+	
+	public void dayMes(int dia, int mes) {
+		this.setDia(dia);
+		this.setMes(mes);
+	}
+
+}
---
+++ src/Principal.java
@@ -0,0 +1,11 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		DataInvertida ame = new DataInvertida(1980, 10, 05);
+		System.out.println(ame);
+
+	}
+
+}
